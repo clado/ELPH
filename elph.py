76a2b9a9c4e4c15097a09c7d979470e83b4212f5
Arie Van Antwerp <arievanantwerp@gmail.com>
2015-05-29 22:05:22 -0500

    Added pruning method with entropy calculations

--- elph.py
+++ elph.py
@@ -1,19 +1,21 @@
+from math import log
 
 class ELPHStream():
 
-  def __init__(self):
-    self.stream = ''
-    self.histogram = {}
+  def __init__(self, hypothesis_threshold=.5):
+    self.__stream = ''
+    self.__histogram = {}
+    self.__threshold = hypothesis_threshold
 
   def record(self, event):
 
     # generate all subsets of current stream
     # such subsets can be represented as binary strings the length of the stream
-    for pattern in range(1, 2**len(self.stream)):
+    for pattern in range(1, 2**len(self.__stream)):
 
-      subset = list(self.stream)
+      subset = list(self.__stream)
       # check which bits in the binary string are turned on
-      for i in range(len(self.stream)):
+      for i in range(len(self.__stream)):
         if pattern ^ 2**i:
           # replace on bits with wildcards
           subset[i] = '*'
@@ -22,26 +24,43 @@ class ELPHStream():
       subset = ''.join(subset).lstrip('*')
       if len(subset) > 0:
         # new subset
-        if subset not in self.histogram.keys():
-          self.histogram[subset] = { 'count': 1, 'frequency': { event: 1} }
+        if subset not in self.__histogram.keys():
+          self.__histogram[subset] = { 'count': 1, 'frequency': { event: 1} }
         # old subset and new event
-        elif event not in self.histogram[subset]:
-          self.histogram[subset]['count'] += 1
-          self.histogram[subset]['frequency'][event] = 1
+        elif event not in self.__histogram[subset]:
+          self.__histogram[subset]['count'] += 1
+          self.__histogram[subset]['frequency'][event] = 1
         # existing subset and existing event
         else:
-          self.histogram[subset]['count'] += 1
-          self.histogram[subset]['frequency'][event] += 1
+          self.__histogram[subset]['count'] += 1
+          self.__histogram[subset]['frequency'][event] += 1
 
     # add event to stream and restrict stream to seven elements max
-    self.stream = self.stream + event
-    if (len(self.stream) > 7):
-      self.stream = self.stream[-7:]
+    self.__stream = self.__stream + event
+    if (len(self.__stream) > 7):
+      self.__stream = self.__stream[-7:]
 
   # TODO:
   def predict(self):
     pass
 
+  def __prune(self):
+    subsets = self.__histogram.keys()
+    for subset in subets:
+      if self.__shannon_entropy(self.__histogram[subset]) > hypothesis_threshold:
+        del self.__histogram[subset]
+
+  def __shannon_entropy(self, histogram):
+    entropy = 0
+    for event in histogram.frequency:
+      entropy += (histogram.frequency[event] / histogram.count) * log(histogram.frequency[event] / histogram.count)
+    return entropy * -1
+
+  def __reliable_entropy(self, histogram):
+    entropy = log(1 / histogram.count) / histogram.count
+    for event in histogram.frequency:
+      entropy += (histogram.frequency[event] / histogram.count) * log(histogram.frequency[event] / histogram.count)
+    return entropy * -1
 
 s = ELPHStream()
 s.record('s')
